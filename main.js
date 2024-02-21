9b528ce6fc5168bf80933e859a8887a019e11866
Maxime <maxime.rache@gmail.com>
2024-02-21 22:29:39 +0100

    tests divers

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } )
 const cube = new THREE.Mesh( geometry, material )
 scene.add( cube )
 
-
 camera.position.z = 5
 
 function animate() {
@@ -55,3 +54,33 @@ scene.add( line )
 renderer.render( scene, camera )
 
 */
+
+
+/* TESTS DIVERS
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize( window.innerWidth, window.innerHeight )
+document.body.appendChild( renderer.domElement )
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 )
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } )
+const cube = new THREE.Mesh( geometry, material )
+
+camera.position.z = 5
+
+scene.add( cube )
+
+function animate() {
+
+    requestAnimationFrame( animate )
+
+    cube.rotation.x += 0.002
+    cube.rotation.y += 0.002
+
+    renderer.render( scene, camera )
+}
+
+animate()*/
